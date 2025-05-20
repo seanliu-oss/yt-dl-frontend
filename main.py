f85b319c1c7a2cd15f16d53b17ffe67ff80cc473
sean.liu <sean.liu.2004@gmail.com>
2025-05-19 22:07:14 -0400

    implement foreground servie

--- main.py
+++ main.py
@@ -1,59 +1,108 @@
-from __future__ import unicode_literals
-
-import threading
-
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
-from kivy.factory import Factory
-from kivy.properties import StringProperty
-from kivy.properties import NumericProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.utils import platform
-from kivy.clock import mainthread
-
+import threading
 import os
 import yt_dlp
-
+from plyer import notification
 
 class Root(BoxLayout):
-    """
-    class Root
-        class that defines the root widget
-        screen layout defined in yt_dl_frontend.kv
-        A pasteButton will paste clipboard to urlInput field
-        A clearButton will clear the urlInput field
-        The download path is default to /download on windows or /sdcard/download on Android
-        Once downButton is pressed, a new thread will be started and invoke youtube_dl
-        to start downloading
-        Properties:
-            saveDir: where to save file
-            downStatus: Current status of downloading
-            progNum: percentage downloaded
-    """
-    _down_path = os.path.abspath('/tmp')
-    if platform == 'win':
-        _down_path = os.path.abspath('/download')
-    if platform == 'android':
-        _down_path = os.path.abspath('/sdcard/download')
-    try:
-        os.mkdir(_down_path)
-    except Exception:
-        pass
-    saveDir = StringProperty(_down_path)
+    saveDir = StringProperty("")
     downStatus = StringProperty("Waiting for URL")
     progNum = NumericProperty(0)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.is_android = platform == "android"
+        if self.is_android:
+            from android.runnable import run_on_ui_thread
+            from android import mActivity
+        else:
+            self.run_on_ui_thread = lambda x: x()
+
+    def start_foreground_service(self):
+        if self.is_android:
+            self.run_on_ui_thread(self._start_foreground_service)()
+
+    def stop_foreground_service(self):
+        if self.is_android:
+            self.run_on_ui_thread(self._stop_foreground_service)()
+
+    def _start_foreground_service(self):
+        from android import api_version
+        from android.permissions import Permission, request_permissions
+
+        def callback(permissions, results):
+            if all(results):
+                print("All permissions granted")
+                self._start_foreground()
+            else:
+                print("Some permissions denied")
+
+        if api_version < 33:
+            permissions = [Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE]
+        else:
+            permissions = [Permission.READ_MEDIA_VIDEO, Permission.READ_MEDIA_AUDIO]
+        request_permissions(permissions, callback)
+
+    def _start_foreground(self):
+        from android import mActivity, autoclass, cast
+        from jnius import JavaException
+
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        Intent = autoclass('android.content.Intent')
+        Context = autoclass('android.content.Context')
+        PendingIntent = autoclass('android.app.PendingIntent')
+        NotificationCompatBuilder = autoclass('androidx.core.app.NotificationCompat$Builder')
+        NotificationManagerCompat = autoclass('androidx.core.app.NotificationManagerCompat')
+
+        self.mServiceIntent = Intent(mActivity.getApplicationContext(), PythonActivity)
+        self.mServiceIntent.setAction("org.test.yt_dl_frontend.FOREGROUND")
+        self.mServicePendingIntent = PendingIntent.getActivity(mActivity, 0, self.mServiceIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        channel_id = "yt_dl_channel"
+        channel_name = "YT-DL Channel"
+        channel_description = "YT-DL Channel Description"
+        NotificationManager = autoclass('android.app.NotificationManager')
+        NotificationChannel = autoclass('android.app.NotificationChannel')
+        notification_service = cast(NotificationManager, mActivity.getSystemService(Context.NOTIFICATION_SERVICE))
+        channel = NotificationChannel(channel_id, channel_name, NotificationManager.IMPORTANCE_LOW)
+        channel.setDescription(channel_description)
+        notification_service.createNotificationChannel(channel)
+
+        builder = NotificationCompatBuilder(mActivity, channel_id)
+        builder.setContentTitle("YT-DL Frontend")
+        builder.setContentText("Downloading...")
+        builder.setSmallIcon(mActivity.getApplicationInfo().icon)
+        builder.setContentIntent(self.mServicePendingIntent)
+        builder.setOngoing(True)
+        self.notification = builder.build()
+
+        notification_manager = NotificationManagerCompat.from_(mActivity)
+        notification_manager.notify(1, self.notification)
+
+        try:
+            mActivity.startService(self.mServiceIntent)
+        except JavaException as e:
+            print("Error starting service:", e)
+
+    def _stop_foreground_service(self):
+        from android import mActivity
+        from android.content import Intent
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        Intent = autoclass('android.content.Intent')
+        Context = autoclass('android.content.Context')
+        NotificationManagerCompat = autoclass('androidx.core.app.NotificationManagerCompat')
+
+        notification_manager = NotificationManagerCompat.from_(mActivity)
+        notification_manager.cancel(1)
+
+        self.mServiceIntent = Intent(mActivity.getApplicationContext(), PythonActivity)
+        self.mServiceIntent.setAction("org.test.yt_dl_frontend.STOP_FOREGROUND")
+        mActivity.stopService(self.mServiceIntent)
+
     def download(self, url, audioOnly, checkSSL):
-        """
-        download(self,url,audioOnly,checkSSL)
-        The main download function, will be started in a separate thread
-        Arguments:
-            url: URL to download
-            audioOnly: A boolean that defines whether to keep audio only
-            checkSSL: A boolean that defines whether to check SSL cert validity
-            Calls the debug, warning and error functions for logging
-            Calls prog_book to report download status
-        """
         ydl_opts = {
             'logger': self,
             'progress_hooks': [self.prog_hook],
@@ -61,15 +110,13 @@ class Root(BoxLayout):
             'ignoreerrors': True,
             'updatetime': False,
             'nocheckcertificate': True
-            # 'restrictfilenames': True,
         }
         if audioOnly:
             ydl_opts['format'] = 'bestaudio[ext=m4a]'
         if checkSSL:
             ydl_opts['nocheckcertificate'] = False
-        if self._down_path:
-            ydl_opts['outtmpl'] = '/'.join([self._down_path, ydl_opts['outtmpl']])
-            print("Output Template:" + ydl_opts['outtmpl'])
+        if self.saveDir:
+            ydl_opts['outtmpl'] = os.path.join(self.saveDir, ydl_opts['outtmpl'])
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
@@ -80,30 +127,15 @@ class Root(BoxLayout):
         pass
 
     def error(self, msg):
-        """
-        error(self,msg)
-            A logging function for youtube_dl, will display the error status
-            Argument: msg passed in by youtube-dl
-        """
-        print(msg)
         self.downStatus = "Oops something went wrong..." + msg[:300]
         self.ids['downButton'].disabled = False
 
-    @mainthread
     def prog_hook(self, d):
-        """
-        prog_hook(self,d)
-            Progress hood for the download
-            Use @mainthread decorator to make sure it updates GUI
-            Updates when the progress exceed multiples of 10 to reduce visual updates
-            Updates progNum property which will be picked up by progBar
-
-        """
         if d['status'] == 'downloading':
             self.downStatus = "Downloading..."
             self.ids['downButton'].disabled = True
             self.percentDown = int(d['_percent_str'].split('.')[0]) // 10 * 10
-            if self.percentDown > self.previousPercentDown:
+            if self.percentDown > getattr(self, 'previousPercentDown', 0):
                 progTxt = '{} percent Downloaded'.format(d['_percent_str'])
                 print(progTxt)
                 self.progNum = self.percentDown
@@ -113,14 +145,10 @@ class Root(BoxLayout):
             self.downStatus = "Finished..."
             self.previousPercentDown = 0
             self.progNum = 100
+            self.stop_foreground_service()
         self.ids['downButton'].disabled = False
 
     def start_download(self, *args):
-        """
-        start_download(self, *args)
-            function that gets called when downButton is pressed
-            Will start a new thread to download
-        """
         urlInput = self.ids['urlInput']
         url = urlInput.text
         self.previousPercentDown = 0
@@ -128,15 +156,24 @@ class Root(BoxLayout):
         audioOnly = self.ids['audio_only_chkbox'].active
         checkSSL = self.ids['check_cert'].active
         self.ids['downButton'].disabled = True
+        self.start_foreground_service()
         threading.Thread(target=self.download, args=(url, audioOnly, checkSSL)).start()
 
+    def on_stop(self):
+        self.stop_foreground_service()
 
 class yt_dl_frontendApp(App):
-    pass
-
+    def build(self):
+        root = Root()
+        if platform == 'android':
+            from android import mActivity
+            _down_path = os.path.abspath('/sdcard/download')
+            try:
+                os.mkdir(_down_path)
+            except Exception:
+                pass
+            root.saveDir = _down_path
+        return root
 
 if __name__ == "__main__":
-    Factory.register('Root', cls=Root)
     yt_dl_frontendApp().run()
-    # root = Root()
-    # download("https://www.youtube.com/watch?v=dQw4w9WgXcQ", False, False)
